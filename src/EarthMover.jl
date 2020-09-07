bf0be94b05972808a7a7d928f563948a9c95bbdc
Adam Paxton <adampaxton9973@gmail.com>
2020-09-07 18:41:47 +0100

    export psolve_monge

--- src/EarthMover.jl
+++ src/EarthMover.jl
@@ -1,7 +1,7 @@
 module EarthMover
 
 export solve_kantorovich, solve_monge, solve_sinkhorn,
-csolve_kantorovich, csolve_monge, csolve_sinkhorn
+csolve_kantorovich, csolve_monge, csolve_sinkhorn, psolve_monge
 
 include("kantorovich_solver.jl")
 include("monge_solver.jl")
